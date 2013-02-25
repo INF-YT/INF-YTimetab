4716bdcf8d81094b72cf7d558f12648b664e9902
Ben Jeffrey <mail@benjeffrey.net>
2013-02-25 17:40:44 +0000

    First commit, included calendar-generation script and lectures ical folder.

---
+++ calendar.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import os.path
+import sys
+
+
+def main(event_directory, course_codes, output_file=''):
+    """
+    Produce an ical calendar containing all the events for the given
+    course-code list, using the individual lecture calendars stored in
+    the event-directory.
+    """
+
+    filename_list = []
+    for filename in os.listdir(event_directory):
+        for code in course_codes:
+            if filename.startswith(code):
+                filename_list.append(str(os.path.join(event_directory, filename)))
+
+    out = sys.stdout
+    if output_file != '':
+        out = open(output_file, 'w')
+    out.write("BEGIN:VCALENDAR\nVERSION:2.0\n")
+
+    for filename in filename_list:
+        f = open(filename, 'r')
+        # remove the first, second, and last lines
+        lines = f.readlines()[2:-1]
+        for line in lines:
+            out.write(line)
+
+    out.write("END:VCALENDAR")
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Produce an ical calendar containing all the events for the given course-codes, using the individual lecture events stored in the event-directory.')
+    parser.add_argument('-d', '--event-directory', default='lectures',
+        help='the directory where ical event files are stored')
+    parser.add_argument('course-code', nargs='+',
+        help='course codes consisting of 4 letters and 5 numbers, i.e. MATH08058')
+    parser.add_argument('-f', '--file', help='output filename')
+    args = parser.parse_args()
+
+    event_directory = './lectures'
+    if args.event_directory:
+        event_directory = args.event_directory
+    course_codes = vars(args)['course-code']
+
+    main(event_directory, course_codes)
