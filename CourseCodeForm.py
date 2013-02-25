35452e28ba03c118c236613df1db7164b706660b
Ben Jeffrey <mail@benjeffrey.net>
2013-02-25 23:25:18 +0000

    Built Flask app.

---
+++ CourseCodeForm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from wtforms import Form, TextField, validators, FieldList, SubmitField
+
+
+class CourseCodeForm(Form):
+    """
+    Class for holding the given course codes.
+    """
+
+    course_codes = FieldList(TextField(
+        label='course-code',
+        validators=[validators.Regexp('[\w]{4}[\d]{5}')]),
+        min_entries=2
+    )
+
+    submit_button = SubmitField('Generate Calendar')
---
+++ app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, request, render_template
+from icalendar_generator import main as generate_ical
+from CourseCodeForm import CourseCodeForm
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html', title='hey there!')
+
+
+@app.route('/calendar', methods=['GET', 'POST'])
+def calendar():
+    form = CourseCodeForm(request.form)
+
+    if request.method == 'POST':
+        ical = generate_ical('static/lectures/', form.course_codes.data, 'str')
+        return (ical, 200, {'Content-Type': 'text/calendar'})
+
+    # by default, render the template with the form:
+    return render_template('course-code-form.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
