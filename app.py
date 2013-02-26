a8810b16860d7145cedd619f7ba95f5878a25c32
Ben Jeffrey <mail@benjeffrey.net>
2013-02-26 22:59:24 +0000

    Edited app.py not to run in debug mode.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from CourseCodeForm import CourseCodeForm
 from flask import Flask, request, render_template
 from icalendar_generator import main as generate_ical
-from CourseCodeForm import CourseCodeForm
+import os
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def calendar():
@@ -13,7 +15,7 @@ def calendar():
     if request.method == 'POST':
         print form.course_codes.data
         course_codes = [field for field in form.course_codes.data if field != '']
-        ical = generate_ical('static/lectures/', form.course_codes.data, 'str')
+        ical = generate_ical('static/lectures/', course_codes, 'str')
         return (ical, 200, {'Content-Type': 'text/calendar'})
 
     # by default, render the template with the form:
@@ -21,4 +23,7 @@ def calendar():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
