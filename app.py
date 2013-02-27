29066aa670a7afca2e5edc74a80f9322705a5701
Ben Jeffrey <mail@benjeffrey.net>
2013-02-27 01:20:44 +0000

    Added /calendar endpoint to post form.

--- app.py
+++ app.py
@@ -9,17 +9,19 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def calendar():
+def index():
     form = CourseCodeForm(request.form)
-
-    if request.method == 'POST':
-        print form.course_codes.data
-        course_codes = [field for field in form.course_codes.data if field != '']
-        ical = generate_ical('static/lectures/', course_codes, 'str')
-        return (ical, 200, {'Content-Type': 'text/calendar'})
-
-    # by default, render the template with the form:
     return render_template('course-code-form.html', form=form)
+
+
+@app.route('/timetable', methods=['GET', 'POST'])
+def timetable():
+    form = CourseCodeForm(request.form)
+    course_codes = [field for field in form.course_codes.data if field != '']
+    if len(course_codes) == 0:
+        return 'No course codes entered.', 204
+    ical = generate_ical('static/lectures/', course_codes, 'str')
+    return (ical, 200, {'Content-Type': 'text/calendar'})
 
 
 if __name__ == '__main__':
